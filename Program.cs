db84ac8f15008a7b3522defb2547c08cef3624cc
unknown <nkosimova06@gmail.com>
2020-03-30 21:34:22 +0500

    second text

--- Program.cs
+++ Program.cs
@@ -20,8 +20,8 @@ namespace Project1
             }
             Console.WriteLine($"result = {summ}");
             }
-            {
-                        
+
+            {           
             Console.WriteLine("------------------ Задача 2.2 --------------------------");
             Console.WriteLine("Даны 2 числа A и B(A>B). Выведите все нечетные числа расположенных между ними");
             int A, B, i;
@@ -35,8 +35,86 @@ namespace Project1
                     Console.Write($"{i} ");
                 }    
             }
-
+            Console.Write("\n");
             }
+
+            {
+            Console.WriteLine("------------------ Задача 3.1 --------------------------");
+            Console.WriteLine("Нарисовать прямоугольник: \n");
+            int i, j;
+            int weight = 5;
+            int len = 20;
+            for (i = 1; i <= weight; i++) {
+                for (j = 1; j <= len; j++){
+                    if (j == 1 || j == len || i == 1 || i == weight) {
+                        Console.Write("*");
+                    } else {
+                        Console.Write(" ");
+                    }
+                }
+                Console.Write("\n");
+            }
+            }
+                        
+            {
+            Console.WriteLine("------------------ Задача 3.2 --------------------------");
+            Console.WriteLine("Нарисовать прямоугольный треугольник: \n");
+            int i, j;
+            int h = 10;
+            for (i = 1; i <=  h; i++) {
+                for (j = 1; j <= i; j++){
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+                }
+            }
+
+            {
+            Console.WriteLine("------------------ Задача 3.3 --------------------------");
+            Console.WriteLine("Нарисовать равносторонний треугольник: \n");
+            int i, j;
+            int h, m = 15;
+            h = m / 2 + 1;
+            for (i = 0; i < h; i++) {
+                for (j = 1; j <= m; j++){
+                    if (j == m/2+1+i || j == m/2+1-i || i == h-1){
+                        Console.Write("*");
+                    } else {
+                        Console.Write(" ");
+                    }                
+                }
+                Console.Write("\n");
+            }
+            }
+
+                    {
+            Console.WriteLine("------------------ Задача 3.3 --------------------------");
+            Console.WriteLine("Нарисовать равносторонний треугольник: \n");
+            int i, j;
+            int h, m = 20;
+            h = m / 2 + 1;
+            for (i = 0; i < h-4; i++) {
+                for (j = 1; j <= m; j++){
+                    if (j == m/2+1+i || j == m/2+1-i || i == h-2){
+                        Console.Write("*");
+                    } else {
+                        Console.Write(" ");
+                    }                
+                }
+                Console.Write("\n");
+            }
+            for (i = h - 4; i >= 0; i--) {
+                for (j = 1; j <= m; j++){
+                    if (j == m/2+1+i || j == m/2+1-i || i == h-2){
+                        Console.Write("*");
+                    } else {
+                        Console.Write(" ");
+                    }                
+                }
+                Console.Write("\n");
+            }
+            }
+            Console.ReadKey();
+        }
         }
     }
-}
